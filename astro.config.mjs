d00a6b7569294398a7e4706da968b298a6297c87
Jim Drury <jim.drury@virginmediao2.co.uk>
2023-01-14 16:26:36 +0000

    feat: adds sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,22 @@
 import {defineConfig} from 'astro/config';
+import sitemap from "@astrojs/sitemap";
 import storyblok from "@storyblok/astro";
-
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://geometricpanda.dev',
     integrations: [
         storyblok({
             accessToken: "li4bleLNJHRanaQG5lydNgtt",
             components: {
-                page: "storyblok/Page",
+                page: "storyblok/Page"
             }
         }),
+        sitemap(),
     ],
     vite: {
         ssr: {
-            noExternal: [ '@astro-community/astro-embed-youtube' ]
+            noExternal: ['@astro-community/astro-embed-youtube']
         }
     }
 });
